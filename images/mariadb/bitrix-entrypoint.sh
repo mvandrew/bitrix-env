be4b796e90c4321ae7c6545ce5157bad42484b42
Andrey Mishchenko <msav@msav.ru>
2021-08-26 19:55:16 +0300

    #2 В образ MariaDB добавлены переменные для настройки объёма используемой памяти

---
+++ images/mariadb/bitrix-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "[mysqld]" >> /etc/mysql/conf.d/zzz-bitrix-ext.cnf \
+    && echo "innodb_buffer_pool_size=${innodb_buffer_pool_size}" >> /etc/mysql/conf.d/zzz-bitrix-ext.cnf \
+    && echo "innodb_buffer_pool_instances=${innodb_buffer_pool_instances}" >> /etc/mysql/conf.d/zzz-bitrix-ext.cnf \
+    && echo "innodb_log_file_size=${innodb_log_file_size}" >> /etc/mysql/conf.d/zzz-bitrix-ext.cnf
+
+exec "$@"
---
+++ images/mariadb/get-mysqltuner.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget http://mysqltuner.pl/ -O ./mysqltuner.pl --no-check-certificate
+chmod 775 ./mysqltuner.pl

10fdf0f65dc83a47ff2a0b9e0e221fa8a38c194c
Andrey Mishchenko <msav@msav.ru>
2021-10-28 14:30:02 +0300

    feature/CU-1pbwjx3-php72-dockerfile-up Первая сборка образа

---
+++ images/php-7.2-apache/bitrix-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Конифиг PHP
+echo "[php]" > "${PHP_CONFIG_FILE_EX}" \
+    && echo "memory_limit = ${MEMORY_LIMIT}" >> "${PHP_CONFIG_FILE_EX}"
+
+# Проверка необходимости залить установщик Битрикс
+BITRIX_PATH="/var/www/html"
+BITRIX_INDEX="${BITRIX_PATH}/index.php"
+BITRIX_SETUP="${BITRIX_PATH}/bitrixsetup.php"
+if [ ! -f $BITRIX_INDEX ] && [ ! -f $BITRIX_SETUP ]; then
+    wget https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php -O $BITRIX_SETUP
+fi
+
+# Обновление прав доступа к файлам
+#chown -R www-data:www-data $BITRIX_PATH
+#chmod 775 $(find ${BITRIX_PATH} -type d)
+#chmod 664 $(find ${BITRIX_PATH} -type f)
+
+exec "$@"

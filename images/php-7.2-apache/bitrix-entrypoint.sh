5ae183c4089056faa66e23043b5aff768cc5884d
Andrey Mishchenko <msav@msav.ru>
2021-11-12 13:59:39 +0300

    feature/CU-1rvyekx-opcache-disable-flag Добавлен флаг контроля запуска модуля opcache для php 7.2

--- images/php-7.2-apache/bitrix-entrypoint.sh
+++ images/php-7.2-apache/bitrix-entrypoint.sh
@@ -12,6 +12,11 @@ if [ ! -f $BITRIX_INDEX ] && [ ! -f $BITRIX_SETUP ]; then
     wget https://www.1c-bitrix.ru/download/scripts/bitrixsetup.php -O $BITRIX_SETUP
 fi
 
+# Контроль запуска модуля opcache
+echo "[opcache]" > "${PHP_CONFIG_FILE_EX}" \
+    && echo "opcache.enable = ${PHP_EX_OPCACHE_ENABLED}" >> "${PHP_CONFIG_FILE_EX}" \
+    && echo "opcache.enable_cli = ${PHP_EX_OPCACHE_ENABLED}" >> "${PHP_CONFIG_FILE_EX}"
+
 # Обновление прав доступа к файлам
 chown -R www-data:www-data $BITRIX_PATH
 #chmod 775 $(find ${BITRIX_PATH} -type d)
